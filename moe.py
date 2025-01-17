85e621275b11d599cc7d8ba940123d582ce2dfa7
Rishi Iyer <79063239+rishiiyer01@users.noreply.github.com>
2025-01-17 14:31:55 -0500

    added topk to sigMoE

--- moe.py
+++ moe.py
@@ -18,7 +18,7 @@ class MLP(nn.Module):
     return self.layer(x)
 
 
-#single linear MoE layer, no regularization
+#single linear MoE layer, no regularization, just topk=1
 class MoE(nn.Module):
 
   def __init__(self, num_experts, hidden_size, ffn_hidden_size):
@@ -50,3 +50,43 @@ class MoE(nn.Module):
     output=p.unsqueeze(-1)*out
 
     return output
+  
+
+#topk sigmoid moe layer, no entropy regularization or loss output
+class sigMoE(nn.Module):
+  def __init__(self, num_experts, hidden_size, ffn_hidden_size,k=1):
+    super().__init__()
+
+    
+    self.k=k
+    self.num_experts = num_experts
+    self.router = nn.Linear(hidden_size, num_experts)
+    self.experts = nn.ModuleList([MLP(hidden_size, ffn_hidden_size) for _ in range(num_experts)])
+    
+
+
+  def forward(self, x):
+    # x: (B, S, H)
+    B,S,H=x.shape
+    scores = self.router(x) # (B, S, E)
+    probs_0 = F.sigmoid(scores) # (B, S, E)
+    p,expert_id=torch.topk(probs_0,self.k,dim=-1) #(B,S,K)
+    p=p.unsqueeze(-1) #B,S,K,1
+    out=torch.zeros((B,S,self.k,H))
+    
+    
+    for i, expert in enumerate(self.experts):
+      #mask=(expert_id==i) #3 dimensional slice (B,S,k)
+      B,S,K=torch.where(expert_id==i)
+      print(x[B,S,:].shape)
+      
+      out[B,S,K,:]=expert(x[B,S,:])
+      print(out.shape,p.shape)
+    output=(p*out).sum(dim=2)
+    
+    return output
+  
+#x = torch.randn((2, 3, 5))
+#moe = sigMoE(4, 5, 20,k=2)
+#print(x)
+#print(moe(x))
